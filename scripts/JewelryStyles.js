43c453406da6a8e85bbdb0ab51dbb965f9613b0c
Ben Shelton <bks-44@hotmail.com>
2022-06-23 16:02:20 -0500

    inital commit

---
+++ scripts/JewelryStyles.js
@@ -0,0 +1,36 @@
+import { getStyles } from "./database.js"
+
+const styles = getStyles()
+
+document.addEventListener(
+    "change",
+    (event) => {
+    }
+)
+
+export const JewelryStyles = () => {
+    let html = "<ul>"
+
+    // Use .map() for converting objects to <li> elements
+    const listItemsArray = styles.map(style => {
+        return `<li>
+            <input type="radio" name="style" value="${style.id}" /> ${style.style}
+        </li>`
+    })
+
+
+    // Join all of the strings in the array into a single string
+    html += listItemsArray.join("")
+
+    html += "</ul>"
+    return html
+}
+
+document.addEventListener(
+    "change",
+    (event) => {
+        if (event.target.name === "style") {
+            setMetal(parseInt(event.target.value))
+        }
+    }
+)
